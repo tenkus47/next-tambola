24bedc3d0b2768efd05f41008ebadb6031a527ab
tenzin kunsang <tenkus47@gmail.com>
2021-09-07 20:02:32 +0530

    last

--- comps/OpeningMessage.js
+++ comps/OpeningMessage.js
@@ -6,12 +6,12 @@ className=' text-center max-w-xl bg-green-300 p-2 mb-2'
 >
 <h4 className='text-xl text-red-600 font-bold'>IMPORTANT NOTICE</h4>
 
-<p>Yak Online Tambola is completely legal (except in cases of Assam, Odisha and Telangana) and has been designed by strictly following the set of rules stated by Indian Governing Law and Jurisdiction.  Participation can only be done by agents for this game.</p>
+<p className='text-sm'>Yak Online Tambola is completely legal (except in cases of Assam, Odisha and Telangana) and has been designed by strictly following the set of rules stated by Indian Governing Law and Jurisdiction.  Participation can only be done by agents for this game.</p>
 
 <div className='text-l text-blue-600'>BEWARE OF FAKE WEBSITES IN THE NAME OF Yak Online Tambola.
 THE OFFICIAL WEBSITE OF Yak Online Housie is This.</div>
 <h3 className='text-xl text-purple-600 font-bold'>!! ANNOUNCEMENT !!</h3>
-<p>
+<p className='text-xs'>
 The company will redraw the game in case of server fault or any technical problem on the server side during the draw.
 In case of redraw, no payments will be made on behalf of the faulting draw.
 Cancellation of tickets will not be entertained, If there is a redraw in the game.
