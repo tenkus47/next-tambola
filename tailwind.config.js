4357d62c167ee827eac0aba5893e1f334e217c1d
tenzin kunsang <tenkus47@gmail.com>
2021-09-07 19:55:27 +0530

    first17

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
-  darkMode: 'class',
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-      extend: {},
+    extend: {},
   },
   variants: {
-      extend: {},
+    extend: {},
   },
   plugins: [],
 }
