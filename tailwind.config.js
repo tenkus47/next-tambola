f3f848c9b2f37b9254be14dd4ea359cc826f2891
tenzin kunsang <tenkus47@gmail.com>
2021-09-07 18:46:23 +0530

    first8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './public/**/*.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
