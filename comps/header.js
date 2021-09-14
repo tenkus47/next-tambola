4435378005c052b1aae092004c3f0443a58c53ab
tenzin kunsang <tenkus47@gmail.com>
2021-09-15 02:05:13 +0530

    done sone7

--- comps/header.js
+++ comps/header.js
@@ -1,12 +1,13 @@
 import styles from '../styles/Home.module.css'
 import Nav from '../comps/nav'
+import Image from 'next/image'
 const Header=({open})=>{
     return(
         <header className={styles.header}>
           <div >
           </div>
             <div className={styles.logo}>
-              <img src='/img/yaklogo.png' style={{height:50,width:50}} alt='imagelogo'/>
+              <Image src='/img/yaklogo.png' height={50} width={50} alt='imagelogo'/>
               <h3 className='font-serif'> YAK ONLINE TAMBOLA </h3>
             </div>
                     
